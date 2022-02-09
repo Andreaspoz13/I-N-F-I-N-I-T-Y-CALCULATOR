9be23ed88ff36c8b5f37294d58482caf04d6ee48
Andreaspoz13 <99254946+Andreaspoz13@users.noreply.github.com>
2022-02-09 09:11:07 +0200

    FILES FOR INFINITY CALCULATOR
    YOU NEED TO DOWNLOAD THOSE FILES AND THEN FOLLOW THE INSTRUCTIONS TO OPEN THE INFINITY CALCULATOR!

---
+++ infinity counter/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace infinity_counter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 9999999; i++)//LOOP!
+            
+            {
+                Console.WriteLine("HOLD OR PRESS ENTER TO COUNT UNTIL I N F I N I T Y."); //INSTRUCTIONS
+                Console.ReadKey();        //WAIT AND DON'T CLOSE WINDOW
+                Console.WriteLine(i+1);  
+                Console.ReadKey();
+                if (int 1 = 1; i<= 9999999;)
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("YOU REACHED I N F I N I T Y !");
+            }
+            
+        }
+    }
+}
